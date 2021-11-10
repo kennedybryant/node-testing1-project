a1d449cc7c783726ce7d80d7eac1717e06e6fc44
kennedybryant <kennedy_marisa96@yahoo.com>
2021-11-10 17:21:33 -0500

    Exercise 5 Done

--- index.js
+++ index.js
@@ -87,6 +87,8 @@ class Seasons {
    */
   constructor() {
     // ✨ initialize whatever properties are needed
+    this.seasons = ['summer', 'fall', 'winter', 'spring']
+    this.currentSeason = 0
   }
 
   /**
@@ -103,6 +105,13 @@ class Seasons {
    */
   next() {
     // ✨ implement
+    const result = this.seasons[this.currentSeason]
+    if (this.currentSeason === 3) {
+      this.currentSeason = 0
+    }else {
+      this.currentSeason++
+    }
+    return result
   }
 }
 
